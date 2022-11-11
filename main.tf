5d5c97195dda9573e3b04989fcc7e3bb58bc91be
RAIMUNDO NEVES <raimneves@gmail.com>
2022-11-11 13:24:27 -0300

    first commit3

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "cloudbuild-ray"
+  name         = "cloudbuildterraform"
   machine_type = "f1-micro"
   tags = ["prod"]
 
